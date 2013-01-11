86e8011672758c30475c9faf9b43616a130c836e
Kako Botasso <kakodev@gmail.com>
2013-01-11 01:42:50 -0200

    Project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mylinks::Application.config.secret_token = '8a4d3ffa2178438e96de04b3919a1450baf1d6ed20fb39c8e1c913a48b0b587fffeefbfd720bc27f58f1c21c806f1ef6ff9b81e8c9cdeae17f700d14bf26167e'
