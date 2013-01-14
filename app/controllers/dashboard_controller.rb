53886b47e1a39a1f7b6a4ef24abb74e7dc74f473
Kako Botasso <kakodev@gmail.com>
2013-01-14 01:04:09 -0200

    Applying layout

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
 	before_filter :require_logged_user
 	
 	def index
-		@links = Link.all
+		@facebook = Link.find_all_by_category(0)
+		@gag = Link.find_all_by_category(1)
+		@article = Link.find_all_by_category(2)
 	end
 end
