a99bd48248c59f77077defa34ec3c690eb477094
Kako Botasso <kakodev@gmail.com>
2013-01-14 23:16:27 -0200

    Adding new categories

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,8 +2,10 @@ class DashboardController < ApplicationController
 	before_filter :require_logged_user
 	
 	def index
-		@facebook = Link.find_all_by_category(0)
-		@gag = Link.find_all_by_category(1)
-		@article = Link.find_all_by_category(2)
+		@facebook = Link.find_all_by_user_id_and_category(session[:user_id], 0)
+		@gag = Link.find_all_by_user_id_and_category(session[:user_id], 1)
+		@article = Link.find_all_by_user_id_and_category(session[:user_id], 2)
+		@technology = Link.find_all_by_user_id_and_category(session[:user_id], 3)
+		@other = Link.find_all_by_user_id_and_category(session[:user_id], 4)
 	end
 end
