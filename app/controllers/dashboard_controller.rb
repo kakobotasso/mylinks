3581a916f7d183fc7f57899c5a87109031d6a138
Kako Botasso <kakodev@gmail.com>
2013-02-08 22:24:50 -0200

    Changing exhibition order

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,10 +2,10 @@ class DashboardController < ApplicationController
 	before_filter :require_logged_user
 	
 	def index
-		@facebook = Link.find_all_by_user_id_and_category(session[:user_id], 0)
-		@gag = Link.find_all_by_user_id_and_category(session[:user_id], 1)
-		@article = Link.find_all_by_user_id_and_category(session[:user_id], 2)
-		@technology = Link.find_all_by_user_id_and_category(session[:user_id], 3)
-		@other = Link.find_all_by_user_id_and_category(session[:user_id], 4)
+		@facebook = Link.find_all_by_user_id_and_category(session[:user_id], 0).sort! { |a,b| b.id <=> a.id }
+		@gag = Link.find_all_by_user_id_and_category(session[:user_id], 1).sort! { |a,b| b.id <=> a.id }
+		@article = Link.find_all_by_user_id_and_category(session[:user_id], 2).sort! { |a,b| b.id <=> a.id }
+		@technology = Link.find_all_by_user_id_and_category(session[:user_id], 3).sort! { |a,b| b.id <=> a.id }
+		@other = Link.find_all_by_user_id_and_category(session[:user_id], 4).sort! { |a,b| b.id <=> a.id }
 	end
 end
