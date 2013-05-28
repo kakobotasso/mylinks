8e9f90133d35c19186fb8ffe98702d39cda79daa
Kako Botasso <kakodev@gmail.com>
2013-05-27 22:46:12 -0300

    Adding validation of uniqueness on Links

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,4 +2,5 @@ class Link < ActiveRecord::Base
   attr_accessible :category, :link, :name, :user_id
 
   validates_presence_of :name, :category, :link, :user_id
+  validates_uniqueness_of :link, scope: :user_id
 end
