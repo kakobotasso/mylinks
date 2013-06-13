2bf3e81acbf536a3689d038f1c8afe17bd5401d5
Kako Botasso <kakodev@gmail.com>
2013-06-13 16:18:58 -0300

    Creating methods to avoid duplications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def html_category(collection, name)
+		html = ""
+		if collection.any?
+			html += "<div class='categoryBar'><span>#{name}</span></div>"
+			html += '<div class="contentLinks">'
+			html += '<ul>'
+			collection.each do |l|
+				html += "<li>#{link_to l.name, l.link, :target => '_blank'} </li>"
+			end
+			html += '</ul>'
+			html += '</div>'
+		end
+		raw html
+	end
 end
